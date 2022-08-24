b671c7be8241576d89e36b3b0f85b07d7dcd1dae
ybh <1845628955@qq.com>
2022-08-24 23:24:07 +0800

    人力资源项目初始化

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+// 引入所有svg目录下的.svg后缀的文件到项目中
